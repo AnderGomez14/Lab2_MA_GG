cc8d7ba77fcf7fa4b197b04bfad340de570671bf
AnderGomez14 <anderelmejor10@gmail.com>
2021-02-10 13:09:55 +0100

    prueba

--- Lab2_MA_GG/inicio.aspx
+++ Lab2_MA_GG/inicio.aspx
@@ -4,13 +4,14 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            Email</div>
+            Email<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
     </form>
 </body>
 </html>
--- Lab2_MA_GG/inicio.aspx.designer.cs
+++ Lab2_MA_GG/inicio.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Lab2_MA_GG
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
     }
 }
